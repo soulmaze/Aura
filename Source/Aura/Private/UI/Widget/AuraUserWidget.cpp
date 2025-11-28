90a689c78f04afcfe4086add43eee6b499090e78
Soulmaze <solmaz.pfz@gmail.com>
2025-11-28 10:34:54 +0100

    adding UI system. adding Aura userWidget class and Aura widget controller class.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Soulmaze Compony
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
